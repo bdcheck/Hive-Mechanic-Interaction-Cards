7987ddea9da3f825af3602ad275a3b07a8719503
Chris J. Karr <chris@audacious-software.com>
2020-12-04 12:29:11 -0600

    Updating process response logging

--- process_response/evaluate.py
+++ process_response/evaluate.py
@@ -38,7 +38,7 @@ result['details'] = {}
 result['actions'] = []
 result['next_id'] = None
 
-logger.info('PROCESS RESP: %s', response)
+logger.info('PROCESS RESP: %s -- %s', response, str(type(response)))
 
 if response is not None:
     result['details']['response'] = response
