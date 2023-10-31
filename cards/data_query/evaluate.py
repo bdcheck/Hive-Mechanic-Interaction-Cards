3e44ff729cb2f81ebe0ec15f402c2a4316afa661
Chris J. Karr <chris@audacious-software.com>
2023-10-31 02:26:20 +0000

    PyLint fix

--- cards/data_query/evaluate.py
+++ cards/data_query/evaluate.py
@@ -45,7 +45,7 @@ if processor is not None:
         }
 
         try:
-            local_env.update(data_processor_environment)
+            local_env.update(data_processor_environment) # pylint: disable=undefined-variable
         except: # nosec # pylint: disable=bare-except
             pass
 
